5776c8605895e3437284f6d1d6058eb36d51f808
William Meng <williammeng1234@gmail.com>
2021-02-02 14:20:07 -0600

    random debug

--- jenkins/scripts/slack_req.sh
+++ jenkins/scripts/slack_req.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-curl -X POST -H 'Content-type: application/json' --data @./jenkins/scripts/slack1.json https://hooks.slack.com/services/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
+echo "HAHA"
+# curl -X POST -H 'Content-type: application/json' --data @./jenkins/scripts/slack1.json https://hooks.slack.com/services/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
