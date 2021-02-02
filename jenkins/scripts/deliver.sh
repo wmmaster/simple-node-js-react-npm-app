c9c301eda61ea58de57a339bfc38992618e5e041
William Meng <williammeng1234@gmail.com>
2021-02-01 22:28:53 -0600

    Add Slack webhook

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -27,3 +27,5 @@ echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
